6edef23e2516c7773db64c4cbd243e1e270b4e23
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-08 22:49:27 -0300

    feat(home): import header on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { type NextPage } from "next";
+import { Header } from "~/components/header";
 import { MiniHeader } from "~/components/miniHeader";
 
 const Home: NextPage = () => {
   return (
     <div>
       <MiniHeader />
+      <Header />
     </div>
   );
 };
