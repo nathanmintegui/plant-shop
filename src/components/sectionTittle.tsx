d8b6646c986d847c9e6153fb6664db247bbfc179
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-10 23:54:34 -0300

    style(tittle): add margin y betwen sections

--- src/components/sectionTittle.tsx
+++ src/components/sectionTittle.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const Tittle: React.FC<Props> = ({ tittle }: Props) => {
   return (
-    <h1 className="inline border-b-black text-center font-unna text-5xl text-tittle-black">
+    <h1 className="my-5 text-center font-unna text-5xl text-tittle-black">
       {tittle}
     </h1>
   );
