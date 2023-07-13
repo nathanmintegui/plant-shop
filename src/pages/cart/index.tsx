e300d33162c1e7c57fec1ef9bb34deaff18d4ffc
nathanmintegui <nathanberger.rs@hotmail.com>
2023-07-13 19:05:12 -0300

    chore(cart-page): create page parts

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,29 @@
+import { Footer } from "~/components/footer";
+import { Header } from "~/components/header";
+import { MiniFooter } from "~/components/miniFooter";
+import { MiniHeader } from "~/components/miniHeader";
+
+const CartItems = () => {
+  return;
+};
+
+const LoginSection = () => {
+  return;
+};
+
+const BillingDetails = () => {
+  return;
+};
+
+const Cart = () => {
+  return (
+    <div>
+      <MiniHeader />
+      <Header />
+      <MiniFooter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Cart;
