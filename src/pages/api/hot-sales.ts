59ed87628f5e920ddf44c2d83fb04af370bf1bd6
nathanmintegui <nathanberger.rs@hotmail.com>
2023-09-10 14:17:47 -0300

    feat(prisma): add server side endpoints

---
+++ src/pages/api/hot-sales.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const plants = await prisma.plant.findMany({
+            where:
+                { isHotSale: true }
+        })
+
+        res.json(plants)
+    } catch (e) {
+        return NextResponse.json({ message: "GET error", e }, { status: 500 })
+    }
+}
+
---
+++ src/pages/api/service.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const plants = await prisma.service.findMany()
+
+        res.json(plants)
+    } catch (e) {
+        return NextResponse.json({ message: "GET error", e }, { status: 500 })
+    }
+}
